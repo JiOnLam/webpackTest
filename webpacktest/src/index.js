ae5d0126eacb3d84b56aabd83d6150e6e7d7f21d
JiOnLam <1214368672@qq.com>
2020-12-09 20:22:18 +0800

    first

---
+++ webpacktest/src/index.js
@@ -0,0 +1,8 @@
+import index from './index.css';
+import indexless from './index.less';
+import indexsass from './index.scss';
+import img1 from './Redmi 8A.png';
+var div = document.querySelector(".img");
+var img = document.createElement("img");
+img.src = img1;
+div.appendChild(img)
---
+++ webpacktest/src/product.js
@@ -0,0 +1,4 @@
+var arr = [10,20,30,40];
+arr.forEach((v,i)=>{
+     console.log(i,v)
+})
